463d33bb06838a46f493ec2b8fb497e5e193b982
Ashutosh Govind Rao <govindraoashutosh@gmail.com>
2026-09-06 13:05:34 +0530

    modified js

---
+++ script.js
@@ -0,0 +1,10 @@
+const cityInput = document.getElementById("cityInput");
+ const cityName = document.getElementById("cityname");
+ const temperature = document.getElementById("temperature");
+ const condition = document.getElementById("condition");
+ const humidity = document.getElementById("humidity");
+ const wind = document.getElementById("wind");
+
+
+ const API_URL = "bdf2e6c8ef7fc9790e4214841aebc61d";
+ 
