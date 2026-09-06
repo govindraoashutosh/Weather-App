781bf45f08172671dce43d0d64f5e8483247b68b
Ashutosh Govind Rao <govindraoashutosh@gmail.com>
2026-09-06 15:50:17 +0530

    fixed errors in js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const cityInput = document.getElementById("cityInput");
- const cityName = document.getElementById("cityname");
+ const cityName = document.getElementById("cityName");
  const temperature = document.getElementById("temperature");
  const condition = document.getElementById("condition");
  const humidity = document.getElementById("humidity");
@@ -7,20 +7,20 @@ const cityInput = document.getElementById("cityInput");
  const searchBtn = document.getElementById("searchBtn");
 
 
- const API_Key = "bdf2e6c8ef7fc9790e4214841aebc61d";
+ const API_KEY = "bdf2e6c8ef7fc9790e4214841aebc61d";
 
 
  async function getWeather(){
   const city = cityInput.value;
   const API_URL  = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
-    const response = await fetch("APi_URL");
-const data = await response.json;
+    const response = await fetch(API_URL);
+const data = await response.json();
 
 cityName.textContent = data.name;
-temperature.textContent = data.temperature + "°C";
-condition.textContent = data.description;
-humidity.textContent = data.humidity + '%';
-wind.textContent = data.wind + "m/s";
+temperature.textContent = data.main.temp + "°C";
+condition.textContent = data.weather[0].description;
+humidity.textContent = data.main.humidity + '%';
+wind.textContent = data.wind.speed + "m/s";
 
  }
 
